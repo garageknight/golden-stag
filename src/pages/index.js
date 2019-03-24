50cc4a62377eaf691a39fbf2a7d92a95b9763e26
Tom Odell <9707773+garageknight@users.noreply.github.com>
2019-03-24 01:57:32 +0000

    added coming soon message

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,16 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO
+      title="Home"
+      keywords={[`gatsby`, `application`, `react`, `goldy`, `golden`, `stag`]}
+    />
+    <h1>STAAAAAAGG!!</h1>
+    <p>Details coming soon...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
